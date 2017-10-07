504c9d6b9d6ef220c6f58726359350621a6109c4
Aaron <aa07.mac@gmail.com>
2017-10-07 06:16:43 -0230

    commented code

--- gotables.go
+++ gotables.go
@@ -8,15 +8,23 @@ import (
 )
 
 func main() {
+	//seeding random to the current time ensures it will be reasonably random
 	rand.Seed( time.Now().UTC().UnixNano())
+	//commandline arguments
 	uptoInt := flag.Int("i", 12, "The highest integer in the times table")
 	flag.Parse()
+	//int i will store the user input
 	var i int
+	//main loop. Continues until Ctrl-C
 	for {
+		//set the random factors
 		x := rand.Intn(*uptoInt)
 		y := rand.Intn(*uptoInt)
+		//print the equation
 		fmt.Printf("%dx%d = ",x ,y )
+		//get user's answer
 		fmt.Scan(&i)
+		//check user's answer
 		if i != x*y {
 			fmt.Printf("Nope. %dx%d = %d\n", x, y, x*y)
 		}
